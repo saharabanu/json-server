437c0e98f3b87f47143e019a83eead42232e8aab
saharabanu <saharabanungn@gmail.com>
2022-09-18 09:56:42 +0600

    todos added

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const { videos, tags } = require("./db_videos.json");
-const {transactions} = require('./db_transactions.json')
+const {transactions} = require('./db_transactions.json');
+const {todos} = require("./db_todos.json")
 
 module.exports = {
     videos,
     tags,
-    transactions
+    transactions,
+    todos
+
 };
