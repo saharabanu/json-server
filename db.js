d2eaea9f3702877420b8e4351b0613ad19c51725
saharabanu <saharabanungn@gmail.com>
2023-11-23 16:34:45 +0600

    added

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
 const { videos, tags } = require("./db_videos.json");
-const {transactions} = require('./db_transactions.json');
-const {todos} = require("./db_todos.json");
-const {users, projects, teams} = require("./db_projects_management.json");
+const { transactions } = require("./db_transactions.json");
+const { todos } = require("./db_todos.json");
+const { users, projects, teams } = require("./db_projects_management.json");
 
 module.exports = {
-    videos,
-    tags,
-    transactions,
-    todos
-
-
+  videos,
+  tags,
+  transactions,
+  todos,
 };
